858c36e063f5e7338fa01e299051489aa5441c95
Oleg-Ivnv <oleg.ivanov@waites.net>
2026-05-28 23:14:31 +0300

    chore: setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: true,
+    origin: [process.env.CLIENT_HOST],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
